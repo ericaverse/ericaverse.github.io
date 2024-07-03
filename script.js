b1b7fed3ec3baa0a4d7e88cb589c60b0870c55a9
Erica Barnett <135877885+ericaverse@users.noreply.github.com>
2024-07-03 18:14:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -42,9 +42,9 @@ span.onclick = function () {
 };
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function (event) {
+/*window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
     setTimeout(carousel, 4000);
   }
-};
+};*/
